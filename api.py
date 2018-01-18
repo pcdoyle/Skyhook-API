06bb488eefbf9beb31a61ee0070c118840a76550
Patrick Doyle <accounts@pcdoyle.com>
2018-01-17 18:40:31 -0800

    Added 8-Ball

--- api.py
+++ api.py
@@ -146,6 +146,16 @@ def conch():
     responses = ["Maybe someday.",  "Nothing.", "Neither.", "I don't think so.", "Yes.", "Try asking again."]
     return Response(random.choice(responses), mimetype='text/plain')
 
+@app.route('/8ball')
+def eightball():
+    """Returns a random response from SpongeBob's Magic Conch shell."""
+    responses = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes definitely.', 
+                 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.',
+                 'Signs point to yes.', 'Reply hazy try again.', 'Ask again later.',
+                 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.',
+                 'Don\'t count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
+    return Response(random.choice(responses), mimetype='text/plain')
+
 @app.errorhandler(404)
 def page_not_found(error):
     """404 Error Handling function. Just returns a plaintext error."""
